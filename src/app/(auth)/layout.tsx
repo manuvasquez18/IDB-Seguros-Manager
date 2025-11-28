b26a12a73c764ddbe6479bb15e5fee90f8715fbf
Luis M Vasquez <lvasquez@clinicaidb.com.ve>
2025-11-28 15:15:12 +0000

    Logo del login https://citasprevimedicaidb.com/sia/assets/img/Logos-en-V

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,11 +10,11 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       <div className="hidden bg-primary lg:flex items-center justify-center p-12">
         <div className="flex flex-col items-center justify-center h-full text-primary-foreground text-center">
             <Image
-                src="https://idbclinicas.com/wp-content/uploads/2017/09/IDB-clinicas-logo.png"
-                alt="IDB Clínicas Logo"
-                width={200}
+                src="https://citasprevimedicaidb.com/sia/assets/img/Logos-en-Vectores-DIAPO.png"
+                alt="IDB Seguros Logo"
+                width={250}
                 height={100}
-                className="mb-4 w-48 h-auto"
+                className="mb-4 w-64 h-auto"
             />
             <h1 className="text-4xl font-bold mt-4">IDB Seguros Manager</h1>
             <p className="mt-4 text-lg">La solución integral para la gestión de seguros.</p>
