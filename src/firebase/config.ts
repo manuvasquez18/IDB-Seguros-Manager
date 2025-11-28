9c3d74758a720bc2d893ad8f6c21e770a2bd70d4
Luis M Vasquez <lvasquez@clinicaidb.com.ve>
2025-11-28 13:20:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7881175649-866f1",
+  "appId": "1:342973538814:web:43481ae018312d24dfe0af",
+  "apiKey": "AIzaSyD0Xso20U58B4CGG3Xz0DQkxOmltd5_RBU",
+  "authDomain": "studio-7881175649-866f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342973538814"
+};
